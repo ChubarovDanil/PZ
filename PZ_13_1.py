06ca1245131a867b6cbdfb95e740c4e4f307a4f9
ChubarovDanil <dcubarov34@gmail.com>
2025-07-01 09:41:15 +0300

    Add files via upload

---
+++ PZ_13_1.py
@@ -0,0 +1,21 @@
+def replace_last_column(matrix):
+    for row in matrix:
+        if row: 
+            row[-1] = -1
+    return matrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print("Исходная матрица:")
+for row in matrix:
+    print(row)
+
+result = replace_last_column(matrix)
+
+print("\nМатрица после замены последнего столбца:")
+for row in result:
+    print(row)
