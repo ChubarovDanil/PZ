5894745eac341f2032484ffe5dae5c1c2f003022
ChubarovDanil <dcubarov34@gmail.com>
2025-10-01 09:53:57 +0300

    Update PZ_12_2.py

--- PZ_12_2.py
+++ PZ_12_2.py
@@ -1,3 +1,4 @@
+#Составить генератор (yield), который преобразует все буквенные символы в строчные
 def lowercase_generator(input_sequence):
     """Генератор, преобразующий все буквенные символы в строчные"""
     for item in input_sequence:
@@ -10,4 +11,5 @@ input_data = ['Hello', 'WORLD', 42, 'PyThOn', '123', 'СъЕШЬ ещё этих
 result = list(lowercase_generator(input_data))
 
 print("Исходные данные:", input_data)
+
 print("После преобразования:", result)
